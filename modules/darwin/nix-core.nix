c6b9cc37a7ebb3e183ae6c5d0e0df9b6b68d2742
ISNing <isning_sf@outlook.com>
2026-05-13 00:07:20 +0800

    fix(darwin/nix-core): disable conflicting Nix management options

--- modules/darwin/nix-core.nix
+++ modules/darwin/nix-core.nix
@@ -13,15 +13,6 @@
   #
   ###################################################################################
 
-  # Determinate uses its own daemon to manage the Nix installation that
-  # conflicts with nix-darwin's native Nix management. so we should disable this option.
-  nix.enable = false;
-
-  # Disable auto-optimise-store because of this issue:
-  #   https://github.com/NixOS/nix/issues/7273
-  # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
-  nix.settings.auto-optimise-store = false;
-
   nix.gc.automatic = false;
 
   system.stateVersion = 5;
