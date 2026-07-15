875f1f09af2e9edcabd74d7a3e0f4f28b26bc2c5
ISNing <isning_sf@outlook.com>
2026-07-15 18:52:48 +0800

    feat(saika): enable Sunshine CUDA support

--- hosts/saika/graphics.nix
+++ hosts/saika/graphics.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   hardware.graphics = {
     enable = true;
@@ -76,10 +81,14 @@
     })
   ];
 
-  services.sunshine.settings = {
-    max_bitrate = 20000; # in Kbps
-    # NVIDIA NVENC Encoder
-    nvenc_preset = 3; # 1(fastest + worst quality) - 7(slowest + best quality)
-    nvenc_twopass = "full_res"; # quarter_res / full_res.
+  services.sunshine = {
+    package = pkgs.sunshine.override { cudaSupport = true; };
+    settings = {
+      max_bitrate = 20000; # in Kbps
+      # NVIDIA NVENC Encoder
+      nvenc_preset = 3; # 1(fastest + worst quality) - 7(slowest + best quality)
+      nvenc_twopass = "full_res"; # quarter_res / full_res.
+    };
   };
+
 }
